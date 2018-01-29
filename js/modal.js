70a9073f3d89f0533fd1da118a4d7c0b37f91f76
alexcorcho <alexander.corcho@gmail.com>
2018-01-29 16:15:20 -0500

    todas las paginas html creas y copiado el codigo inicial

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,6 @@
 function modal(_valor){
     document.getElementById("bgventana").style.visibility=_valor;
 }
+
+// para ejecutar script dentro de una etiqueta a//
+//<a href="javascript:modal('visible');" ><img class="icon" src="img/icon1.png" alt="uno" ></a>//
