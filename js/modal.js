efb790490a05274c03798255d5a2eba9dd176e1e
Alexander Corcho <alexander.corcho@gmail.com>
2018-02-22 12:32:32 -0500

    se realiza campio por completo

--- js/modal.js
+++
@@ -1,6 +0,0 @@
-function modal(_valor){
-    document.getElementById("modal").style.visibility=_valor;
-}
-
-// para ejecutar script dentro de una etiqueta a//
-//<a href="javascript:modal('visible');" ><img class="icon" src="img/icon1.png" alt="uno" ></a>//
